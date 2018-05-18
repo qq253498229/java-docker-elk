9de7a2c6ebfbeefd3e5e60e7ecfacde27b06efe1
大胃王 <253498229@qq.com>
2018-05-18 16:17:34 +0800

    update

--- app/src/main/java/com/example/app/AppApplication.java
+++ app/src/main/java/com/example/app/AppApplication.java
@@ -1,13 +1,15 @@
 package com.example.app;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
 
 /**
  * @author wangbin
@@ -17,16 +19,38 @@ import java.util.Date;
 @Slf4j
 public class AppApplication {
 
-  @Scheduled(cron = "*/10 * * * * ?")
-  public void testLogin() {
+  public List<String> users = new ArrayList<>();
+  public List<String> operations = new ArrayList<>();
+
+  /**
+   * 每秒执行一次
+   */
+  @Scheduled(cron = "*/1 * * * * ?")
+  public void test() {
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    log.info("用户登录:" + sdf.format(new Date()));
+    log.info(sdf.format(new Date()) + "用户" + getRandomUser() + "执行" + getRandomOperation() + "操作");
   }
 
-  @Scheduled(cron = "*/30 * * * * ?")
-  public void testLogout() {
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    log.info("用户退出:" + sdf.format(new Date()));
+  /**
+   * 随机获取用户
+   */
+  private String getRandomUser() {
+    return users.get(new Random().nextInt(users.size()));
+  }
+
+  /**
+   * 随机获取操作
+   */
+  private String getRandomOperation() {
+    return operations.get(new Random().nextInt(operations.size()));
+  }
+
+  @Bean
+  CommandLineRunner runner() {
+    return args -> {
+      users.addAll(Arrays.asList("刘大", "钱二", "张三", "李四", "王五", "赵六", "田七"));
+      operations.addAll(Arrays.asList("登录", "退出", "转账", "收款", "发送消息", "接受消息", "加入群聊", "退出群聊"));
+    };
   }
 
   public static void main(String[] args) {
